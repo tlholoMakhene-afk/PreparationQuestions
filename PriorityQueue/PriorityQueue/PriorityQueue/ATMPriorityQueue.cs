77279f121b50c34c767c6917aa86025052a91a78
Tlholo <58808719+tlholoMakhene-afk@users.noreply.github.com>
2020-11-30 21:09:39 +0100

    Add files via upload

---
+++ PriorityQueue/PriorityQueue/PriorityQueue/ATMPriorityQueue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PriorityQueue
+{
+    partial class Program
+    {
+        public class ATMPriorityQueue<T> where T : IComparable<T>
+        {
+            private Queue<T> data;
+
+            public ATMPriorityQueue()
+            {
+                this.data = new Queue<T>();
+            }
+
+            public override string ToString()
+            {
+                string s = "";
+                for (int i = 0; i < data.Count; ++i)
+                    s += "ATM " + (i+1).ToString()  + " in " + data.ElementAt(i).ToString() + "meters ";
+                return s;
+            }
+
+            public int Count()
+            {
+                return data.Count;
+            }
+
+            public T Dequeue()
+            {
+                return data.Dequeue();
+            }
+
+            public void Enqueue(T item)
+            {
+                data.Enqueue(item);
+            }
+
+            public T Peek()
+            {
+                return data.Peek();
+            }
+           public void Order()
+            {
+                Queue<T> q2 = new Queue<T>();
+                foreach (T i in this.data.OrderBy(x => x))
+                    q2.Enqueue(i);
+
+                this.data = q2;
+            }
+
+        }
+
+    }
+}
---
+++ PriorityQueue/PriorityQueue/PriorityQueue/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriorityQueue
+{
+    partial class Program
+    {
+
+        static void Main(string[] args)
+        {
+            ATMPriorityQueue<double> pq = new ATMPriorityQueue<double>();
+            int num_ATMS = 3;
+            double current_locqtion = 0.0;
+
+            HashSet<KeyValuePair<string, double>> AllATMLocations = new HashSet<KeyValuePair<string, double>>(  )
+            {
+                new KeyValuePair<string, double>( "amt1", 45.2 ),
+                new KeyValuePair<string, double>( "amt2", 80 ),
+                new KeyValuePair<string, double>("amt3", 45.2) ,
+                new KeyValuePair<string, double>("amt4", 64 ),
+                new KeyValuePair<string, double>("amt5", 57 ),
+                new KeyValuePair<string, double>("amt6", 1.00),
+                new KeyValuePair<string, double>("amt7", 35),
+                new KeyValuePair<string, double>("amt8", 78 )
+            };
+
+
+            foreach (var item in AllATMLocations)
+            {
+                if (pq.Count() < num_ATMS)
+                {
+                    pq.Enqueue(getLocation(current_locqtion, item.Value));
+                }
+                else
+                {
+                    if (pq.Peek() > getLocation(current_locqtion, item.Value))
+                    {
+                        pq.Dequeue();
+                        pq.Enqueue(getLocation(current_locqtion, item.Value));
+                    }
+                }
+            }
+
+            Console.WriteLine("Priory queue is: ");
+            pq.Order();
+            Console.WriteLine(pq.ToString());
+            Console.ReadKey();
+        }
+
+        private static double getLocation(double curr, double atm)
+        {
+            return atm - curr;
+        }
+
+    }
+}
